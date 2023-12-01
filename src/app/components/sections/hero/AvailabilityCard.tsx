29094afd6dca5bf18114a84b8a4ffbc5ee633b60
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-01 14:25:21 -0300

    feat: add Availability Card component

---
+++ src/app/components/sections/hero/AvailabilityCard.tsx
@@ -0,0 +1,13 @@
+const AvailabilityCard = () => {
+  return (
+    <div className="flex gap-3 items-center bg-specialOrange/70 font-semibold text-white rounded-md py-1 px-5">
+      <span className="flex h-3 w-3 pointer-events-none relative">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-specialOrange opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-specialOrange"></span>
+      </span>
+      <p className="">Disponível para novos desafios</p>
+    </div>
+  );
+};
+
+export default AvailabilityCard;
