cf1762ae57fefeb04e5b0b4a2c5b0223ac96fcbe
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-01 14:24:06 -0300

    feat: add initial mobile nav component

--- src/app/components/header/MobileNav.tsx
+++ src/app/components/header/MobileNav.tsx
@@ -13,18 +13,29 @@ const MobileNav = () => {
   return (
     <div className="md:hidden">
       <HamburguerIcon onClick={handleMenuToggle} isMenuOpen={isMenuOpen} />
-      {isMenuOpen && (
-        <div className="flex h-screen w-screen fixed top-[80px] right-0 left-0">
-          <div
-            className="bg-black/20 w-[40vw] cursor-pointer"
-            onClick={handleMenuToggle}
-          />
 
-          <nav className="w-[60vw] z-10 bg-white text-black px-5">
-            <NavbarLinks direction="col" extraStyles="justify-left" />
-          </nav>
-        </div>
-      )}
+      <div
+        className={`flex h-[calc(100vw-80px)] w-full overflow-x-hidden fixed ${
+          isMenuOpen ? 'z-[100]' : 'z-[-100]'
+        }`}
+      >
+        <div
+          className={`bg-black/50 z-10 w-[40vw] top-[80px] transition-all duration-300 cursor-pointer fixed inset-0 ${
+            isMenuOpen ? 'opacity-100' : 'pointer-events-none opacity-0'
+          }
+            }`}
+          onClick={handleMenuToggle}
+        />
+        <nav
+          className={`w-[60vw] z-20 bg-white h-full top-[80px] text-black px-5 fixed transition-all duration-300 right-0 ${
+            isMenuOpen
+              ? 'right-0 opacity-100 z-[101]'
+              : '-right-full opacity-0 pointer-events-none'
+          }`}
+        >
+          <NavbarLinks direction="col" extraStyles="justify-left" />
+        </nav>
+      </div>
     </div>
   );
 };
