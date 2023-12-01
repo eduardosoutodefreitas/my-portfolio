0b771e765be0f4a646b0742037e0004484408c73
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-01 14:23:40 -0300

    style: change button hover effect

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -8,8 +8,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = ({ children, extraStyles, ...props }: ButtonProps) => {
   return (
     <button
-      className={`px-5 py-1 text-sm md:text-base lg:text-lg flex items-center gap-3 rounded-md bg-primaryBlack text-white font-semibold 
-      hover:-translate-y-1 transition-all duration-250 ${extraStyles}`}
+      className={`px-5 py-2 text-sm md:text-base lg:text-lg flex items-center gap-3 rounded-md bg-primaryBlack text-white font-semibold 
+      hover:-translate-y-1 transition-all duration-300 hover:bg-specialOrange ${extraStyles}`}
       {...props}
     >
       {children}
