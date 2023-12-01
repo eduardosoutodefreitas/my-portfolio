7024731169ad842a0a94574d927d229f6dd6dfee
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-11-30 21:33:57 -0300

    feat: add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from './components/header/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Freitas portfolio",
-  description: "Front end Developer, React, Typescript, Javascript, Web Developer",
-}
+  title: 'Freitas portfolio',
+  description:
+    'Front end Developer, React, Typescript, Javascript, Web Developer',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
