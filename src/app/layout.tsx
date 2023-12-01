539d3771387238bf1383d8542e687444db19eff5
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-01 14:23:17 -0300

    chore: rename archive and replace hero image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from './components/header/Header';
+import Header from './components/header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`text-primaryBlack ${inter.className}`}>
         <Header />
         {children}
       </body>
