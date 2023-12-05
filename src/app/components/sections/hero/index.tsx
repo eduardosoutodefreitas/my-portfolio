d2e5089e41236a746144357839eb7a4b4582a771
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-04 22:05:02 -0300

    style: change hero height and padding

--- src/app/components/sections/hero/index.tsx
+++ src/app/components/sections/hero/index.tsx
@@ -3,7 +3,7 @@ import HeroPicture from './HeroPicture';
 
 const Hero = () => {
   return (
-    <section className="w-full min-h-[calc(100vh-80px)] flex flex-col md:flex-row items-center justify-center px-3 md:px-8 lg:px-14 py-5 gap-8 lg:gap-12">
+    <section className="w-full lg:min-h-[calc(100vh-80px)] py-20 lg:py-0 flex flex-col md:flex-row items-center justify-center px-3 md:px-8 lg:px-14 gap-8 lg:gap-12">
       <HeroCta />
       <HeroPicture />
     </section>
