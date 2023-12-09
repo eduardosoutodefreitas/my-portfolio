3ce4a17d81b0c3b7ad7de189f5db7395fa792e4f
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-09 18:34:07 -0300

    feat: change hero picure size and source

--- src/app/components/sections/hero/HeroPicture.tsx
+++ src/app/components/sections/hero/HeroPicture.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 const HeroPicture = () => {
   return (
     <Image
-      src={'/assets/images/hero.svg'}
+      src={'/assets/images/hero.png'}
       alt="Hero Icon"
-      width={482}
-      height={598}
+      width={500}
+      height={500}
       // fill
-      className="animate-float aspect-[492/598] w-4/6 min-w-[250px] max-w-2xl pointer-events-none"
+      className="w-1/2 min-w-[250px] xs:sm:min-w-[350px] max-w-2xl pointer-events-none"
     />
   );
 };
