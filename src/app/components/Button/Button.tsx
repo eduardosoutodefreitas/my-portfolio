21c0e18e9091378804dc44f1c045e8020af8c46d
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-14 14:37:11 -0300

    style: change Button text sizing

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = ({ children, extraStyles, ...props }: ButtonProps) => {
   return (
     <button
-      className={`px-5 py-2 text-sm md:text-base lg:text-lg flex items-center gap-3 rounded-md bg-primaryBlack text-white font-semibold 
+      className={`px-5 py-2 text-xs xs:text-sm md:text-base lg:text-lg flex items-center gap-3 rounded-md bg-primaryBlack text-white font-semibold 
       hover:-translate-y-1 transition-all duration-300 hover:bg-specialOrange ${extraStyles}`}
       {...props}
     >
