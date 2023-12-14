a1a74274e617d6e0661ad6a85a9617e878a87585
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-14 18:30:28 -0300

    feat: add About section

--- src/app/components/sections/about/About.tsx
+++ src/app/components/sections/about/About.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import AboutPictures from './AboutPictures';
+import AboutQuote from './AboutQuote';
+import AboutInfos from './AboutInfos';
+import SectionHeading from '../../SectionHeading/SectionHeading';
 
 const About = () => {
   return (
-    <section id="about" className="min-h-screen">
-      About
+    <section
+      id="about"
+      className="min-h-screen container mx-auto flex flex-col py-20 md:py-36 relative px-5 text-primaryBlack"
+    >
+      <SectionHeading title="sobre mim" subtitle="saiba mais" />
+      <AboutQuote />
+      <div className="flex lg:justify-around md:items-start flex-col md:flex-row items-center justify-center md:gap-24 lg:gap-0">
+        <AboutPictures />
+        <AboutInfos />
+      </div>
     </section>
   );
 };
