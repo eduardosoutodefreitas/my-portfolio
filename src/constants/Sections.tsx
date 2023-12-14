054a9f5456f5a3a2e132f7529c8654be4ed3da19
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-14 14:35:57 -0300

    feat: add Sections constant

---
+++ src/constants/Sections.tsx
@@ -0,0 +1,15 @@
+import {
+  IoHome,
+  IoPerson,
+  IoCodeSlash,
+  IoChatboxEllipses,
+} from 'react-icons/io5';
+
+const sections = [
+  { id: '#home', label: 'Home', icon: <IoHome /> },
+  { id: '#about', label: 'About', icon: <IoPerson /> },
+  { id: '#services', label: 'Services', icon: <IoCodeSlash /> },
+  { id: '#contact', label: 'Contact', icon: <IoChatboxEllipses /> },
+];
+
+export default sections;
