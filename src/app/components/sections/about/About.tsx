bf06dab80a71274c4e1f6930381fcdb7ddcdb093
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-14 14:50:37 -0300

    feat: add initial About Section

---
+++ src/app/components/sections/about/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about" className="min-h-screen">
+      About
+    </section>
+  );
+};
+
+export default About;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from './components/sections/hero';
+import About from './components/sections/about/About';
+import Hero from './components/sections/hero/Hero';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
