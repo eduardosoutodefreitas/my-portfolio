60c247825d1c3e0ae5729986cffcfe0681384668
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-21 18:30:33 -0300

    chore: add contentful hostname to next config remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
