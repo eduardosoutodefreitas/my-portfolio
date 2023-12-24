af8b9b0394c1d9da01a9c79065508013880537e3
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-23 22:55:34 -0300

    style: remove not used import

--- types/contentful/TypeProject.ts
+++ types/contentful/TypeProject.ts
@@ -1,4 +1,4 @@
-import type { Asset, Entry, EntryFields } from 'contentful';
+import type { Asset, EntryFields } from 'contentful';
 
 export interface TypeProjectFields {
   name: EntryFields.Symbol;
