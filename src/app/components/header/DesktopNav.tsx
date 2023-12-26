286f8b414b1953d679f0af999dda8fe61e0d49ce
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-26 16:27:00 -0300

    style: change desktopNav component breakpoint

--- src/app/components/header/DesktopNav.tsx
+++ src/app/components/header/DesktopNav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const DesktopNav = () => {
   return (
-    <NavbarContent className="hidden sm:flex gap-5" justify="center">
+    <NavbarContent className="hidden md:flex gap-5" justify="center">
       {sections.map((section) => (
         <NavbarItem key={section.id}>
           <Link color="foreground" href={section.id}>
