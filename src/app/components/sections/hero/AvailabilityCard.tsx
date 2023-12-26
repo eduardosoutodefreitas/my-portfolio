f0fc3551494618e59f82ef3148fd0f553fff9d34
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-26 16:29:59 -0300

    style: change AvailabilityCard component breakpoint and padding x

--- src/app/components/sections/hero/AvailabilityCard.tsx
+++ src/app/components/sections/hero/AvailabilityCard.tsx
@@ -1,11 +1,11 @@
 const AvailabilityCard = () => {
   return (
-    <div className="flex gap-3 items-center bg-specialOrange/70 font-semibold text-white rounded-md py-1 px-5">
+    <div className="flex gap-3 items-center bg-specialOrange/70 font-semibold text-white rounded-md py-1 px-4">
       <span className="flex h-3 w-3 pointer-events-none relative">
         <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-specialOrange opacity-75"></span>
         <span className="relative inline-flex rounded-full h-3 w-3 bg-specialOrange"></span>
       </span>
-      <p className="text-sm md:text-base lg:text-lg">
+      <p className="text-sm lg:text-base xl:text-lg">
         Disponível para novos desafios
       </p>
     </div>
