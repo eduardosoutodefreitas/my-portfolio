065bdc83049850c8a00802be0ae94f102b43f9ec
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-26 15:57:51 -0300

    fix: fix type error at contentful client object

--- src/lib/contentful/client.ts
+++ src/lib/contentful/client.ts
@@ -1,6 +1,6 @@
 const contenful = require('contentful');
 
 export const client = contenful.createClient({
-  space: process.env.CONTENFUL_SPACE_ID!,
-  accessToken: process.env.CONTENFUL_ACCESS_TOKEN!,
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
 });
