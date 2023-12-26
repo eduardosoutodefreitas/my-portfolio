87df5e6149919b2782ddf8013e5103436a0db27b
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-26 16:29:28 -0300

    style: change hero padding y

--- src/app/components/sections/hero/Hero.tsx
+++ src/app/components/sections/hero/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full lg:min-h-screen py-20 lg:py-0 flex flex-col md:flex-row items-center justify-around px-3 md:px-8 lg:px-14 gap-8 lg:gap-12
+      className="w-full lg:min-h-screen py-24 lg:py-0 flex flex-col md:flex-row items-center justify-around px-3 md:px-8 lg:px-14 gap-8 lg:gap-12
     "
     >
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
