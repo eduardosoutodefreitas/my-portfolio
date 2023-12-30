ec223ea04906f4e0805c24085ef275ca33e09d9e
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2023-12-29 21:02:17 -0300

    chore: add Id field to ProjectType

--- types/contentful/TypeProject.ts
+++ types/contentful/TypeProject.ts
@@ -1,11 +1,12 @@
-import type { Asset, EntryFields } from 'contentful';
+import type { Asset, Entry, EntryFields } from 'contentful';
 
 export interface TypeProjectFields {
   name: EntryFields.Symbol;
-  projectHighlight: Asset;
   projectImages: Asset[];
   techStack: EntryFields.Symbol[];
   description: EntryFields.Text;
   projectUrl: EntryFields.Symbol;
   repositoryUrl?: EntryFields.Symbol;
+  projectHighlight: Asset;
+  id: EntryFields.Symbol;
 }
