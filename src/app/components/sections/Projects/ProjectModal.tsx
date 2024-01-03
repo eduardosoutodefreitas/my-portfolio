60ba9fa6ebb3dcc06a3fc218f3e2a40ca2c97db4
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-03 11:13:36 -0300

    feat: add ProjectModal component

---
+++ src/app/components/sections/Projects/ProjectModal.tsx
@@ -0,0 +1,70 @@
+'use client';
+import React from 'react';
+import {
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalHeader,
+  Divider,
+} from '@nextui-org/react';
+import { TypeProjectFields } from '../../../../../types/contentful';
+import ProjectStack from './ProjectStack';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { VscGithub } from 'react-icons/vsc';
+import Link from 'next/link';
+import ModalSlider from './ModalSlider';
+
+interface ProjectModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  project: TypeProjectFields;
+}
+
+const ProjectModal = ({ isOpen, onClose, project }: ProjectModalProps) => {
+  return (
+    <>
+      <Modal
+        backdrop="blur"
+        size={'3xl'}
+        isOpen={isOpen}
+        onClose={onClose}
+        hideCloseButton
+      >
+        <ModalContent>
+          <ModalHeader className="p-0 shadow-md">
+            <ModalSlider projectImages={project.projectImages} />
+          </ModalHeader>
+          <ModalBody className="px-10 py-5">
+            <h3 className="font-bold mt-1 text-2xl">{project.name}</h3>
+            <ProjectStack techStack={project.techStack} />
+            <p className="text-xs xs:text-sm mt-1">{project.description}</p>
+            <Divider />
+            <div className="flex justify-between items-center w-full">
+              <Link
+                href={project.projectUrl}
+                className="flex items-center font-semibold gap-3 group text-specialOrange"
+              >
+                Projeto
+                <FaExternalLinkAlt className="group-hover:translate-x-1 transition duration-250" />
+              </Link>
+              {project.repositoryUrl && (
+                <Link
+                  href={project.repositoryUrl}
+                  className="flex items-center font-semibold gap-3 group"
+                >
+                  Github
+                  <VscGithub
+                    size={22}
+                    className="group-hover:translate-x-1 transition duration-250"
+                  />
+                </Link>
+              )}
+            </div>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProjectModal;
