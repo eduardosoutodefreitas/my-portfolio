48df23895ec168861906e2cd0749630a19b1501e
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-03 11:14:03 -0300

    feat: add ModalSlider component

---
+++ src/app/components/sections/Projects/ModalSlider.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import Image from 'next/image';
+import { Asset } from 'contentful';
+import { Autoplay } from 'swiper/modules';
+interface ModalSliderProps {
+  projectImages: Asset[];
+}
+
+const ModalSlider = ({ projectImages }: ModalSliderProps) => {
+  return (
+    <Swiper
+      spaceBetween={30}
+      slidesPerView={1}
+      centeredSlides
+      modules={[Autoplay]}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      loop={true}
+      className="mySwiper w-full"
+    >
+      {projectImages.map((image, index) => (
+        <SwiperSlide className="w-full" key={index}>
+          <Image
+            src={`https:${image.fields.file?.url}`}
+            alt={image.fields.title as string}
+            height={948}
+            width={1920}
+            className="w-full h-auto"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ModalSlider;
