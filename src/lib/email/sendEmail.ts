de517989b4659f601fbb3196d57a811f22adc1b9
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-03 15:22:02 -0300

    fix: add emailjs.init to sendEmail

--- src/lib/email/sendEmail.ts
+++ src/lib/email/sendEmail.ts
@@ -1,12 +1,12 @@
 import emailjs from '@emailjs/browser';
 import { FormData } from '../validations/schema';
 export const sendEmail = async (templateParams: FormData) => {
+  emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_KEY!);
   try {
     const result = await emailjs.send(
       process.env.NEXT_PUBLIC_SERVICE_ID!,
       process.env.NEXT_PUBLIC_TEMPLATE_ID!,
       templateParams,
-      process.env.NEXT_PUBLIC_EMAILJS_KEY,
     );
 
     console.log(result);
