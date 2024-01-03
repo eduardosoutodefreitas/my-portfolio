4934b89c36dc2fd548553e5081cda8a4768571da
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-03 11:55:56 -0300

    style: add scroll-smooth to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`text-primaryBlack ${inter.className}`}>
         <AppProvider>
           <Header />
